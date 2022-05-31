54aaac04c132dd022b4af4d5d1b4518ad6099022
Ga <gabri.magni.97@gmail.com>
2022-05-31 16:12:27 +0200

    $table in migration

--- database/migrations/2022_05_31_132114_create_comics_table.php
+++ database/migrations/2022_05_31_132114_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('thumb');
+            $table->decimal('price', 5, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
