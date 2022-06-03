4e83218d04db5969db5b54d8d4919fedfa5d0748
Ga <gabri.magni.97@gmail.com>
2022-06-03 10:43:34 +0200

    delete btn

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,6 +9,11 @@
 <h2>{{ $comic->title }}</h2>
 <a href="{{ route('comics.show', $comic->id) }}">Entra</a>
 <a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
+<form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+    @method('DELETE')
+    @csrf
+    <button type="submit">Elimina</button>
+</form>
 
 @endforeach
 
