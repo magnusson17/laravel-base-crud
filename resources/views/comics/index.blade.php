3256f571c51bde84f1057c61078ac03d2e78dee7
Ga <gabri.magni.97@gmail.com>
2022-06-03 14:36:01 +0200

    cards in index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,31 +1,48 @@
 @extends('layout.layout')
 
 @section('content')
+    @if ('message')
+        <div>
+            {{ session('message') }}
+        </div>
+    @endif
 
-@if ('message')
-<div>
-    {{ session('message') }}
-</div>
-@endif
+    <a href="{{ route('comics.create') }}" class="btn btn-success">New</a>
 
-<a href="{{ route('comics.create') }}">Nuovo</a>
+    <div class="container">
+        <div class="row row-cols-4 g-3 justify-content-center">
+            @foreach ($comics as $comic)
+                <div>
+                    <div class="card h_90vh" style="width: 18rem;">
+                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="img">
+                        <div class="card-body overflow-auto">
 
-@foreach ($comics as $comic)
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <p class="card-text">{{ $comic->description }}</p>
 
-<h2>{{ $comic->title }}</h2>
-<a href="{{ route('comics.show', $comic->id) }}">Entra</a>
-<a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
-<form action="{{ route('comics.destroy', $comic->id) }}" method="POST" class="delete-form" data-name="{{ $comic->title }}">
-    @method('DELETE')
-    @csrf
-    <button type="submit">Elimina</button>
-</form>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col d-flex flex-column">
+                                        <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary mb-2">Entra</a>
+                                        <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning mb-2">Modifica</a>
+                                        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" class="delete-form mb-2"
+                                            data-name="{{ $comic->title }}">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="btn btn-danger w-100">Elimina</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
 
-@endforeach
-
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
 
 @section('delete-message')
-<script src="{{ asset('js/deleteMessage.js') }}"></script>
+    <script src="{{ asset('js/deleteMessage.js') }}"></script>
 @endsection
-    
