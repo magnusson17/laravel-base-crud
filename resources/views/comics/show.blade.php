05fc06b35df4a0be21d747b288ed9970780d6c45
Ga <gabri.magni.97@gmail.com>
2022-06-03 14:58:25 +0200

    show php

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
-<h2>{{ $comic->title }}</h2>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>{{ $comic->title }}</h2>
+            <p>{{ $comic->description }}</p>
+            <span>{{ $comic->price }}$</span>
+            <p>{{ $comic->series }}</p>
+        </div>
+    </div>
+</div>
+
     
 @endsection
